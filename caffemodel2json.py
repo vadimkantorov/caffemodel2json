e810a58adbe2383a1525adcdfd7309acb986771b
Vadim Kantorov <vadimkantorov@gmail.com>
2016-10-14 17:39:41 +0200

    Update

--- caffemodel2json.py
+++ caffemodel2json.py
@@ -6,11 +6,6 @@ import argparse
 import tempfile
 import subprocess
 
-def log(x, prefix = None):
-	if prefix != None:
-		x = '%8s: %s' % (prefix, x)
-	print >> sys.stderr, x
-
 def pb2json(pb, print_arrays):
 	from google.protobuf.descriptor import FieldDescriptor as FD
 	_ftype2js = {
@@ -30,21 +25,20 @@ def pb2json(pb, print_arrays):
 		FD.TYPE_SFIXED64: float,
 		FD.TYPE_SINT32: int,
 		FD.TYPE_SINT64: long,
+		FD.TYPE_MESSAGE: lambda x: pb2json(x, print_arrays = print_arrays)
 	}
 	js = {}
 	fields = pb.ListFields()	#only filled (including extensions)
 	for field,value in fields:
-		if field.type == FD.TYPE_MESSAGE:
-			ftype = pb2json
-		elif field.type in _ftype2js:
+		if field.type in _ftype2js:
 			ftype = _ftype2js[field.type]
 		else:
-			log("WARNING: Field %s.%s of type '%d' is not supported" % (pb.__class__.__name__, field.name, field.type, ))
+			print >> sys.stderr, "WARNING: Field %s.%s of type '%d' is not supported" % (pb.__class__.__name__, field.name, field.type, )
 		if field.label == FD.LABEL_REPEATED:
 			js_value = []
 			for v in value:
 				js_value.append(ftype(v))
-			if not print_arrays and (len(js_value) > 64 or (field.name == 'data' and len(js_value) > 8)):
+			if not print_arrays and (field.name == 'data' and len(js_value) > 8):
 				head_n = 5
 				js_value = js_value[:head_n] + ['(%d elements more)' % (len(js_value) - head_n)]
 		else:
@@ -59,27 +53,15 @@ parser.add_argument('--data', help = 'Print all arrays in full', action = 'store
 parser.add_argument('--codegenDir', help = 'Path to an existing temporary directory to save generated protobuf Python classes', default = tempfile.mkdtemp())
 args = parser.parse_args()
 
-log('calling protoc', 'protobuf')
 local_caffe_proto = os.path.join(args.codegenDir, os.path.basename(args.caffe_proto))
 with open(local_caffe_proto, 'w') as f:
 	f.write((urllib2.urlopen(args.caffe_proto) if 'http' in args.caffe_proto else open(args.caffe_proto)).read())
 	
-subprocess.check_call(['protoc', '--proto_path', args.codegenDir, '--python_out', args.codegenDir, local_caffe_proto])
-log('generated', 'protobuf')
-
+subprocess.check_call(['protoc', '--proto_path', os.path.dirname(local_caffe_proto), '--python_out', args.codegenDir, local_caffe_proto])
 sys.path.insert(0, args.codegenDir)
 import caffe_pb2
-log('imported', 'protobuf')
-
 
 netParam = caffe_pb2.NetParameter()
-msg = open(args.model_caffemodel, 'rb').read()
-log('caffemodel read in memory. Deserialization will take a few minutes. Take a coffee!', 'model')
-netParam.ParseFromString(msg)
-log('deserialized', 'model')
+netParam.ParseFromString(open(args.model_caffemodel, 'rb').read())
 
 json.dump(pb2json(netParam, args.data), sys.stdout, indent = 2)
-log('json saved', 'model')
-
-log('')
-log('ALLOK. Quitting')
