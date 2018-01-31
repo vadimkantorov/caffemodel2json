a589214c83389286c0680ba4b99285e338272994
Vadim Kantorov <vadimkantorov@gmail.com>
2018-01-31 09:31:46 +0900

    fixes

--- README.md
+++ README.md
@@ -1,28 +1,24 @@
 # Description
-A small tool to dump Caffe's \*.caffemodel and \*.binaryproto files to JSON for inspection (\*.prototxt files is not needed).
+A small tool to dump Caffe's \*.caffemodel and \*.binaryproto files to JSON for inspection (\*.prototxt files is not needed). By default [caffe.proto](https://raw.githubusercontent.com/BVLC/caffe/master/src/caffe/proto/caffe.proto) from the official Caffe repository is used.
 
 # Usage
-To dump model structure without weights (unless `--data` switch is used) to JSON:
 ```shell
-./caffemodel2json.py CAFFE_ROOT/src/caffe/proto/caffe.proto model_name.caffemodel > dump.json
-```
-or
-```shell
-alias caffemodel2json='python2.7 <(curl -sS https://raw.githubusercontent.com/vadimkantorov/caffemodel2json/master/caffemodel2json.py)'
-caffemodel2json https://raw.githubusercontent.com/BVLC/caffe/master/src/caffe/proto/caffe.proto model_name.caffemodel > dump.json
-```
+# to dump model structure without weights (unless `--data` switch is used) to JSON, using default caffe.proto:
+./caffemodel2json.py model.caffemodel > dump.json
 
-To dump a binaryproto:
-```shell
+# to dump model structure without weights (unless `--data` switch is used) to JSON:
+./caffemodel2json.py model.caffemodel CAFFE_ROOT/src/caffe/proto/caffe.proto > dump.json
+
+# to dump a binaryproto:
 wget http://dl.caffe.berkeleyvision.org/caffe_ilsvrc12.tar.gz
 mkdir -p imagenet_mean && tar -xf caffe_ilsvrc12.tar.gz -C imagenet_mean
-./caffemodel2json.py https://raw.githubusercontent.com/BVLC/caffe/master/src/caffe/proto/caffe.proto imagenet_mean/imagenet_mean.binaryproto --data > imagenet_mean.json
+./caffemodel2json.py imagenet_mean/imagenet_mean.binaryproto --data > imagenet_mean.json
 ```
 
 # Dependencies
 1. Python
 2. protobuf (with Python bindings)
-3. Caffe's caffe.proto (or from a Caffe's fork, it's important if you are inspecting a model that implements new Caffe layers)
+3. (optional) Caffe's caffe.proto (or from a Caffe's fork, it's important if you are inspecting a model that implements new Caffe layers)
 
 # License
 MIT
--- caffemodel2json.py
+++ caffemodel2json.py
@@ -1,57 +1,28 @@
 import os
 import sys
 import json
-import urllib2
 import argparse
 import tempfile
 import subprocess
 from google.protobuf.descriptor import FieldDescriptor as FD
+try:
+	from urllib2 import urlopen
+except:
+	from urllib.request import urlopen
 
-# inspired by https://github.com/dpp-name/protobuf-json/blob/master/protobuf_json.py
-def pb2json(pb, print_arrays):
-	_ftype2js = {
-		FD.TYPE_DOUBLE: float,
-		FD.TYPE_FLOAT: float,
-		FD.TYPE_INT64: long,
-		FD.TYPE_UINT64: long,
-		FD.TYPE_INT32: int,
-		FD.TYPE_FIXED64: float,
-		FD.TYPE_FIXED32: float,
-		FD.TYPE_BOOL: bool,
-		FD.TYPE_STRING: unicode,
-		FD.TYPE_BYTES: lambda x: x.encode('string_escape'),
-		FD.TYPE_UINT32: int,
-		FD.TYPE_ENUM: int,
-		FD.TYPE_SFIXED32: float,
-		FD.TYPE_SFIXED64: float,
-		FD.TYPE_SINT32: int,
-		FD.TYPE_SINT64: long,
-		FD.TYPE_MESSAGE: lambda x: pb2json(x, print_arrays = print_arrays),
-		'unknown' : lambda x: 'Unknown field type: %s' % x
-	}
-	js = {}
-	for field, value in pb.ListFields():
-		ftype = _ftype2js[field.type] if field.type in _ftype2js else _ftype2js['unknown']
-		if field.label == FD.LABEL_REPEATED:
-			js_value = map(ftype, value)
-			if not print_arrays and (field.name == 'data' and len(js_value) > 8):
-				head_n = 5
-				js_value = js_value[:head_n] + ['(%d elements more)' % (len(js_value) - head_n)]
-		else:
-			js_value = ftype(value)
-		js[field.name] = js_value
-	return js
-
-parser = argparse.ArgumentParser('Dump model_name.caffemodel to a file JSON format for debugging')
-parser.add_argument(metavar = 'caffe.proto', dest = 'caffe_proto', help = 'Path to caffe.proto (typically located at CAFFE_ROOT/src/caffe/proto/caffe.proto)')
+parser = argparse.ArgumentParser('Dump model.caffemodel to a file in JSON format for debugging')
 parser.add_argument(metavar = 'model.caffemodel', dest = 'model_caffemodel', help = 'Path to model.caffemodel')
+parser.add_argument('--caffe.proto', metavar = '--caffe.proto', dest = 'caffe_proto', help = 'Path to caffe.proto (typically located at CAFFE_ROOT/src/caffe/proto/caffe.proto)', default = 'https://raw.githubusercontent.com/BVLC/caffe/master/src/caffe/proto/caffe.proto')
 parser.add_argument('--data', help = 'Print all arrays in full', action = 'store_true')
 parser.add_argument('--codegenDir', help = 'Path to an existing temporary directory to save generated protobuf Python classes', default = tempfile.mkdtemp())
 args = parser.parse_args()
 
+contract_array = lambda xs, f, head = 5: xs[:head] + ['({} elements more)'.format(len(xs) - head)] if f.name == 'data' and len(xs) > 8 and not args.data else xs
+to_dict = lambda obj: {f.name : converter(v) if f.label != FD.LABEL_REPEATED else contract_array(list(map(converter, v)), f) for f, v in (obj.ListFields() if obj is not None else []) for converter in [{FD.TYPE_DOUBLE: float, FD.TYPE_SFIXED32: float, FD.TYPE_SFIXED64: float, FD.TYPE_SINT32: int, FD.TYPE_SINT64: long, FD.TYPE_FLOAT: float, FD.TYPE_ENUM: int, FD.TYPE_UINT32: int, FD.TYPE_INT64: long, FD.TYPE_UINT64: long, FD.TYPE_INT32: int, FD.TYPE_FIXED64: float, FD.TYPE_FIXED32: float, FD.TYPE_BOOL: bool, FD.TYPE_STRING: unicode, FD.TYPE_BYTES: lambda x: x.encode('string_escape'), FD.TYPE_MESSAGE: to_dict}.get(f.type, 'Unknown field type: {}'.format)]}
+
 local_caffe_proto = os.path.join(args.codegenDir, os.path.basename(args.caffe_proto))
 with open(local_caffe_proto, 'w') as f:
-	f.write((urllib2.urlopen if 'http' in args.caffe_proto else open)(args.caffe_proto).read())
+	f.write((urlopen if 'http' in args.caffe_proto else open)(args.caffe_proto).read())
 	
 subprocess.check_call(['protoc', '--proto_path', os.path.dirname(local_caffe_proto), '--python_out', args.codegenDir, local_caffe_proto])
 sys.path.insert(0, args.codegenDir)
@@ -60,4 +31,4 @@ import caffe_pb2
 deserialized = caffe_pb2.NetParameter() if os.path.splitext(args.model_caffemodel)[1] == '.caffemodel' else caffe_pb2.BlobProto()
 deserialized.ParseFromString(open(args.model_caffemodel, 'rb').read())
 
-json.dump(pb2json(deserialized, args.data), sys.stdout, indent = 2)
+json.dump(to_dict(deserialized), sys.stdout, indent = 2)
